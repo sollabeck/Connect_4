8c7ca13bf821789b14a0083e5a37695834409ca5
Ganesh Balaji & Kevin C <pair+ganeshkbalaji.sollabeck@devbootcamp.com>
2015-03-26 16:04:21 -0700

    do jquery handling

--- connect4.js
+++ connect4.js
@@ -1,6 +1,34 @@
+$( document ).ready(function() {
+
 //Controller
 
 
 //View
 
 //Model
+
+
+var player = {
+
+}
+
+$("tbody tr:nth-child(1)").children().click(function(event){
+  // player = player1 ? player = player2 : player = player1;
+  if(event.target && event.target.nodeName == "circle"){
+      console.log($(this).index());
+    }
+
+});
+
+});
+
+
+
+
+
+
+
+
+
+
+//First column... $("tbody tr").children("td:nth-child(1)")
